e4a95d1ab46752b09deff9611c0b9f0ef4a27b89
Vladislav Smirnov <vs@comindware.com>
2018-10-26 07:30:16 +0300

    Add vuetify lib.

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,11 @@ import CardContainer from './components/card/CardContainer.vue';
 import ServicesContainer from './components/services/ServicesContainer.vue';
 import SocialContainer from './components/social/SocialContainer.vue';
 import ApplicationStore from './ApplicationStore';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(VueRouter);
+Vue.use(Vuetify);
 
 export default new Vue({
   el: '#vue-app',
