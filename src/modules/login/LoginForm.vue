70b33ce4791a53e1c8686816107fc68f15ec31af
Evgeny Smirnov <Evgeny.Smirnov@comindware.com>
2018-10-30 17:45:35 +0300

    Design login page layout

--- src/modules/login/LoginForm.vue
+++ src/modules/login/LoginForm.vue
@@ -22,7 +22,7 @@
         required
       ></v-text-field>
     </v-form>
-     <v-btn v-on:click="navigateToHome" class="login-button">Войти</v-btn>
+     <v-btn v-on:click="navigateToHome" class="login-button" color="white">Войти</v-btn>
     </div>
     </div>
     </div>
@@ -48,34 +48,50 @@ export default Vue.extend({
 </script>
 
 <style>
+
+.form-container {
+    display: flex;
+    justify-content: center;
+    padding: 25px;
+    height: 100%;
+}
 .login-container {
     flex-direction: column;
     display: flex;
     justify-content: center;
-    margin-top: -30px;
-    margin-bottom: 30px;
+    margin-top: -48px;
     flex: 1;
 }
-.login-title {
-    font-size: 2.5em;
-    width: 100%;
+.picture-container{
     text-align: center;
+    margin-top: -100px;
 }
-.form-container {
-    display: flex;
-    justify-content: center;
-    padding: 10px;
-    height: 100%;
+.login-title {
+    font-size: 1.6em;
+    font-weight: bold;
+    width: 100%;
+    color: #0271b9;
+    text-align: center;
+    margin: 60px 0 50px;
 }
+
 .form-group {
     background-color: #efeff4;
-    border-radius: 15px;
-    padding: 10px;
+    padding: 13px 25px 25px;
     display: flex;
     flex-direction: column;
 }
-.login-button {
-    background-color: white;
-    flex: 1;
+.login-container.login-button,
+.login-container .v-btn:not(.v-btn--depressed):not(.v-btn--flat){
+    text-transform: uppercase;
+    color: #e74d3b;
+    border: 1px solid #e74d3b !important;
+    margin: 20px 0 15px;
+    border-radius: 5px;
+    box-shadow:  0 2px 2px 0 rgba(100,100,100,.14);
+}
+.form-container .login-button .v-btn__content {
+    text-transform: uppercase;
+    color: #e74d3b;
 }
 </style>
