64ad150e748be8a58cb7464804e8595c06fb5565
Dmitry Lyange <dimliang95@gmail.com>
2019-01-29 21:00:38 +0300

    Initial .Net core solution upload

---
+++ src/server/Contracts/Kanban.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Corevue.Data
+{
+    public class KanbanCard
+    {
+        public string Id { set; get; }
+
+        public string Title { set; get; }
+
+        public string Description { get; set; }
+    }
+
+    public class KanbanColumn
+    {
+        public string Id { set; get; }
+
+        public string Title { set; get; }
+
+        public string Description { get; set; }
+
+        public List<KanbanCard> Cards { get; set; }
+    }
+
+    public class KanbanProject
+    {
+        public string Id { set; get; }
+
+        public string Name { set; get; }
+
+        public string Description { get; set; }
+
+        public string Icon { set; get; }
+
+        public DateTime? Date { set; get; }
+
+        public List<KanbanCard> Cards { get; set; }
+    }
+}
---
+++ src/server/CoreView/Controllers/KanbanController.cs
@@ -0,0 +1,60 @@
+﻿using Corevue.Data;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreView.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KanbanController : ControllerBase
+    {
+        /// <summary>
+        /// List
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<KanbanCard>> Get()
+        {
+            return new KanbanCard[] { new KanbanCard(), new KanbanCard() };
+        }
+
+        /// <summary>
+        /// Get
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public ActionResult<KanbanCard> Get(string id)
+        {
+            return new KanbanCard();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        [HttpPost]
+        public void Post([FromBody] KanbanCard value)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        [HttpPut("{id}")]
+        public void Put(string id, [FromBody] KanbanCard value)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+        }
+    }
+}
